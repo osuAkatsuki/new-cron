c99b1eece892c195fc7f7204b8bc7fea4565e25f
cmyui <cmyuiosu@gmail.com>
2023-11-05 06:30:31 -0500

    load .env

--- settings.py
+++ settings.py
@@ -1,8 +1,13 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DB_NAME = os.environ["DB_NAME"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = int(os.environ["DB_PORT"])
 DB_PASS = os.environ["DB_PASS"]
 DB_USER = os.environ["DB_USER"]
+
 DISCORD_AC_WEBHOOK = os.environ["DISCORD_AC_WEBHOOK"]
