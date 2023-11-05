4baa7d54e8a631aa1ab8c0fd99853a6f4862da5d
cmyui <cmyuiosu@gmail.com>
2023-11-05 06:57:50 -0500

    r

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,13 +9,10 @@ if [ -z "$APP_ENV" ]; then
 fi
 
 if [[ $PULL_SECRETS_FROM_VAULT -eq 1 ]]; then
-  # pip install -i $PYPI_INDEX_URL akatsuki-cli
-  # # TODO: revert to $APP_ENV
-  # akatsuki vault get new-cron production-k8s -o .env
-  # source .env
-  # ls .env
-  # pwd
-  # cat .env
+  pip install -i $PYPI_INDEX_URL akatsuki-cli
+  # TODO: revert to $APP_ENV
+  akatsuki vault get new-cron production-k8s -o .env
+  source .env
 fi
 
 # await database availability
