db1381758baab6604902f374032fec06594bd474
James Wilson <tsunyoku@gmail.com>
2024-06-02 13:53:52 +0100

    remove incorrect akatsuki-cli install in bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,6 @@ if [ -z "$APP_ENV" ]; then
 fi
 
 if [[ $PULL_SECRETS_FROM_VAULT -eq 1 ]]; then
-  pip install -i $PYPI_INDEX_URL akatsuki-cli
   # TODO: revert to $APP_ENV
   akatsuki vault get new-cron production-k8s -o .env
   source .env
