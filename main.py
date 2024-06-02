cac3d2c46fea600f783eab761b8fc5d4e6d5b54e
tsunyoku <tsunyoku@gmail.com>
2024-06-02 14:09:08 +0100

    use `aclose`

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def connect() -> None:
 async def disconnect() -> None:
     await db.close()
 
-    await redis.close()
+    await redis.aclose()
 
     print("Disconnected from database and redis")
 
