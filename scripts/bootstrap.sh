f69626abd0ef3ce9e1bf6f33fc167c9e549768b2
Josh Smith <cmyuiosu@gmail.com>
2024-06-16 00:22:07 -0400

    Switch vault to use $APP_ENV

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,8 +9,7 @@ if [ -z "$APP_ENV" ]; then
 fi
 
 if [[ $PULL_SECRETS_FROM_VAULT -eq 1 ]]; then
-  # TODO: revert to $APP_ENV
-  akatsuki vault get new-cron production-k8s -o .env
+  akatsuki vault get new-cron $APP_ENV -o .env
   source .env
 fi
 
